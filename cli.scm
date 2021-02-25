fd24893f6c0c2d7d06f5bcdca455467098ac4dc4
nobody246 <a.se8seven@gmail.com>
2021-02-25 02:07:04 -0500

    Add files via upload

---
+++ cli.scm
@@ -0,0 +1,58 @@
+(define expected-args
+  '(samps-per-sec infile outfile chans volume duration pxMultiple threshold rotationOrientation negative))
+(define current-arg '())
+(define argc (length cli-args))
+(define (usage)
+  (print "Spectropic (c) 2020 Alex S. a.se8seven (a) g...m")
+  (print "GNU Public License v2")
+  (print "=======USAGE:=========")
+  (print
+   "csi spc.scm [samps-per-sec ## in-file foo out-file bar chans # volume .## duration # pxMultiple #]\n"
+   "in-file: image file for input\n"
+   "out-file: wav file for output\n"
+   "chans: channels in wav file\n"
+   "volume: volume (<=1.0) for output sound\n"
+   "samps-per-sec: samples per second in generated wav file (default 11025)\n"
+   "threshold: the minimum combined added total of the RGB values of a pixel for said pixel to be transferred to the output wav file.\n"
+   "duration: duration of wav file 'pixel' at any given Y coordinate that meets threshold criteria along the X axis.\n"
+   "pxMultiple: The multiple for a Y coordinate that meets threshold criteria in the output wav file.\n"
+   "rotationOrientation: The number of times for imlib2 to rotate the input image. It is the parameter of the image-orientate command.\n")
+  (exit))
+(define (parse-cli args)
+  (when (not (null? args))
+    (let ((arg (car args)))
+      (if (= (remainder argc 2) 0)
+	  (let ((param-name arg)
+		(a 0))
+	    (for-each (lambda (x)
+			(when (equal? (symbol->string x) param-name)
+			  (set! a (add1 a))))
+		      expected-args)
+	    (when (= a 0)
+	      (usage))
+	    (set! current-arg (string->symbol param-name)))
+	  (cond ((eq? current-arg 'samps-per-sec)
+		 (set! s-per-sec (string->number arg)))
+                ((eq? current-arg 'infile)
+                 (set! in-file arg))
+                ((eq? current-arg 'outfile)
+                 (set! out-file arg))
+                ((eq? current-arg 'chans)
+                 (set! chans (string->number arg)))
+                ((eq? current-arg 'volume)
+                 (set! volume (string->number arg)))
+                ((eq? current-arg 'duration)
+                 (set! duration (string->number arg)))
+                ((eq? current-arg 'px-multiple)
+                 (set! pixel-multiple (string->number arg)))
+                ((eq? current-arg 'threshold)
+                 (set! threshold (string->number arg)))
+                ((eq? current-arg 'rotationOrientation)
+                 (set! orient (string->number arg)))
+                ((eq? current-arg 'negative)
+                 (if (equal? arg "1")
+                     (set! _OP <=)
+                     (set! _OP >=)))))
+      (set! argc (add1 argc))
+      (parse-cli (cdr args)))))
+
---
+++ spc.scm
@@ -0,0 +1,68 @@
+(use sndfile imlib2)
+(define cli-args (command-line-arguments))
+(include "cli.scm")
+(define 2pi  (* 2 (acos -1.0)))
+(define s-per-sec 11025)
+(define in-file "in.jpg")
+(define out-file "out.wav")
+(define chans 1)
+(define volume .05)
+(define duration 500)
+(define threshold 300)
+(define orient 6)
+(define _OP <=)
+(define pixel-multiple 50)
+(parse-cli cli-args)
+(when (not (file-exists? in-file))
+  (print "error input file not found")
+  (exit))
+(define img
+  (let ((flip (lambda (x) (image-orientate x orient))))
+    (flip (image-load in-file))))
+(define imgw (image-width img))
+(define imgh (image-height img))
+(define (precalc-wave-part freq)
+  (* 2pi freq))
+(define lst '())
+(define (set-current-freqs args)
+  (set! lst
+    (map (lambda (x)
+           (precalc-wave-part x))
+         args)))
+(define (process-img x)
+  (let r ((y 0)
+          (f-list '()))
+    (define-values (_r _g _b _a)
+      (image-pixel/rgba img x y))
+    (when (_OP (+ _r _g _b) threshold)
+      (set! f-list
+        (append f-list `(,(* y pixel-multiple)))))
+    (if (< y (sub1 imgh))
+        (r (add1 y) f-list)
+        (append '(1) f-list))))
+(define (make-wav w h #!optional (c 0))
+  (set-current-freqs (process-img c))
+  (let r ((a 0))
+    (when (< a duration)
+      (w h (list->f32vector
+            `(,(* volume
+                  (apply
+                   +
+                   (map
+                    (lambda (x)
+                      (sin
+                       (/ (* x (+ a c))
+                          s-per-sec)))
+                    lst))))))
+      (r (add1 a))))
+  (when (< c (sub1 imgw))
+    (make-wav w h (add1 c))))
+(with-sound-to-file
+ out-file
+ '(wav pcm-16 file)
+ s-per-sec
+ chans
+ (lambda (h)
+   (make-wav write-items/f32
+             h)))
+(exit)
